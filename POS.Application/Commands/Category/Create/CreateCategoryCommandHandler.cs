2e03254e4a57f333f071282ee815603fdc4d85be
Adejorooluwatobi <adejorotgold1@gmail.com>
2026-04-24 19:53:53 +0100

    roles

--- POS.Application/Commands/Category/Create/CreateCategoryCommandHandler.cs
+++ POS.Application/Commands/Category/Create/CreateCategoryCommandHandler.cs
@@ -12,17 +12,20 @@ public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryComman
     private readonly ICategoryRepository _repository;
     private readonly IUnitOfWork _uow;
     private readonly IMapper _mapper;
+    private readonly ITenantContext _tenantContext;
 
-    public CreateCategoryCommandHandler(ICategoryRepository repository, IUnitOfWork uow, IMapper mapper)
+    public CreateCategoryCommandHandler(ICategoryRepository repository, IUnitOfWork uow, IMapper mapper, ITenantContext tenantContext)
     {
         _repository = repository;
         _uow = uow;
         _mapper = mapper;
+        _tenantContext = tenantContext;
     }
 
     public async Task<CategoryDto> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
     {
         var entity = _mapper.Map<Entity>(request.Dto);
+        entity.TenantId = _tenantContext.TenantId;
         await _repository.AddAsync(entity);
         await _uow.SaveChangesAsync(cancellationToken);
         return _mapper.Map<CategoryDto>(entity);
