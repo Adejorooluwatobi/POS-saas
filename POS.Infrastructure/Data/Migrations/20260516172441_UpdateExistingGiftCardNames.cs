8024988b24c148c233283f2e539e9128a86bd400
Adejorooluwatobi <adejorotgold1@gmail.com>
2026-05-16 18:26:26 +0100

    card

--- POS.Infrastructure/Data/Migrations/20260516172441_UpdateExistingGiftCardNames.cs
+++ POS.Infrastructure/Data/Migrations/20260516172441_UpdateExistingGiftCardNames.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -17,6 +17,8 @@ namespace POS.Infrastructure.Data.Migrations
                 keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
                 column: "UpdatedAt",
                 value: new DateTimeOffset(new DateTime(2026, 5, 16, 17, 24, 37, 771, DateTimeKind.Unspecified).AddTicks(3941), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.Sql("UPDATE \"TransactionItems\" SET \"ProductName\" = 'Gift Card Sale' WHERE \"VariantId\" IS NULL AND \"ProductName\" IS NULL;");
         }
 
         /// <inheritdoc />
