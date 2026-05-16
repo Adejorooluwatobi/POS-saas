ec0eefb93eff02e23c6e8a890df838217f90fc14
Adejorooluwatobi <adejorotgold1@gmail.com>
2026-05-16 18:25:25 +0100

    card

---
+++ POS.Infrastructure/Data/Migrations/20260516172441_UpdateExistingGiftCardNames.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateExistingGiftCardNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tenants",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 5, 16, 17, 24, 37, 771, DateTimeKind.Unspecified).AddTicks(3941), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tenants",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 5, 16, 17, 10, 30, 808, DateTimeKind.Unspecified).AddTicks(3753), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
